9c33c5f61c58ffc8281dd69d07ffd85c3599a542
Gregory Haskins <gregory.haskins@gmail.com>
2015-12-10 22:40:07 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/datomic_test/schema.clj
+++ src/datomic_test/schema.clj
@@ -2,17 +2,25 @@
 
 (def schema
   [{:db/id #db/id[:db.part/db]
-    :db/ident :community/name
+    :db/ident :document/id
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/fulltext true
-    :db/doc "A community's name"
+    :db/unique :db.unique/identity
+    :db/index true
+    :db/doc "A unique identifier for this document"
     :db.install/_attribute :db.part/db}
    {:db/id #db/id[:db.part/db]
-    :db/ident :community/url
-    :db/valueType :db.type/string
+    :db/ident :document/version
+    :db/valueType :db.type/long
     :db/cardinality :db.cardinality/one
-    :db/doc "A community's url"
+    :db/doc "The version of this document after commit"
+    :db.install/_attribute :db.part/db}
+   {:db/id #db/id[:db.part/db]
+    :db/ident :document/entries
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/isComponent true
+    :db/doc "Name/value pair entries attached to this document"
     :db.install/_attribute :db.part/db}
    ]
   )
