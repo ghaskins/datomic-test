e01d190a578792e62dd0f3e5fce91e47654de588
Gregory Haskins <gregory.haskins@gmail.com>
2015-12-11 11:31:12 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/datomic_test/core.clj
+++ src/datomic_test/core.clj
@@ -4,6 +4,7 @@
 
 (use '[datomic.api :only [q db] :as d])
 (use 'datomic-test.schema)
+(use 'clojure.pprint)
 
 (def cli-options
   ;; An option with a required argument
@@ -23,17 +24,42 @@
       (d/transact conn datomic-test.schema/schema)
       conn))
 
+(defn getdoc [id db]
+  (let [result (q '[:find (pull ?doc [*]) :where [?doc :document/id id]] db)
+        nr (count result)]
+    (if (not= 1 nr)
+      (throw (Exception. (format "unexpected result count: %d", nr))))
+    (ffirst result)))
+
 (defn run []
   (let [conn (create-db "datomic:mem://foo")
-        result (d/transact conn [{:db/id #db/id[:db.part/user -1000001],
-                                  :entry/name "bar"
-                                  :entry/value (.getBytes "baz")}
-                                 {:db/id #db/id[:db.part/user],
-                                  :document/id "foo",
-                                  :document/version 1
-                                  :document/entries #db/id[:db.part/user -1000001]}
-                                 ])]
-    (print result)))
+        insresult (d/transact conn [{:db/id #db/id[:db.part/user -1000001],
+                                     :entry/name "bar"
+                                     :entry/value (.getBytes "baz")}
+                                    {:db/id #db/id[:db.part/user -1000002],
+                                     :entry/name "bat"
+                                     :entry/value (.getBytes "bah")}
+                                    {:db/id #db/id[:db.part/user],
+                                     :document/id "foo",
+                                     :document/version 1
+                                     :document/entries [#db/id[:db.part/user -1000001]
+                                                        #db/id[:db.part/user -1000002]]}
+
+                                    {:db/id #db/id[:db.part/user -1000003],
+                                     :entry/name "bar"
+                                     :entry/value (.getBytes "baz")}
+                                    {:db/id #db/id[:db.part/user -1000004],
+                                     :entry/name "bat"
+                                     :entry/value (.getBytes "bah")}
+                                    {:db/id #db/id[:db.part/user],
+                                     :document/id "bar",
+                                     :document/version 1
+                                     :document/entries [#db/id[:db.part/user -1000003]
+                                                        #db/id[:db.part/user -1000004]]}
+                                    ])
+       result (getdoc "foo" (db conn))]
+    (pprint result)
+    ))
 
 (defn -main [& args]
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
